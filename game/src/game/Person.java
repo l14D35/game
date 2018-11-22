3be0ce23e03feb4e9b600174196e7a78ef0f0672
l14d35 <kujawa.dp@gmail.com>
2018-11-22 22:46:53 +0100

    Added Person class

---
+++ game/src/game/Person.java
@@ -0,0 +1,22 @@
+package game;
+
+public abstract class Person {
+	private int health = 0;
+	private int attackDamage = 0;
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getAttackDamage() {
+		return attackDamage;
+	}
+
+	public void setAttackDamage(int attackDamage) {
+		this.attackDamage = attackDamage;
+	}
+}
